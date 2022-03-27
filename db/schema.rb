84dfc3c8fa6a6ec221d206cafbaeaab0d67c7ad9
nyount14 <90732548+nyount14@users.noreply.github.com>
2022-03-27 12:42:18 -0500

    added users table and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_18_002017) do
+ActiveRecord::Schema.define(version: 2022_03_27_172955) do
 
   create_table "transactions", force: :cascade do |t|
     t.float "amount"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 2022_03_18_002017) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "category"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
