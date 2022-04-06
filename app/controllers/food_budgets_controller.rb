f6bc50ade7cc4ea8dea1ea5561ad036c8b3a0f12
nyount14 <90732548+nyount14@users.noreply.github.com>
2022-04-05 20:53:48 -0500

    added the food budget new form

--- app/controllers/food_budgets_controller.rb
+++ app/controllers/food_budgets_controller.rb
@@ -1,15 +1,26 @@
 class FoodBudgetsController < ApplicationController
+    before_action :find_by_id, only: [:show, :edit, :update, :destroy]
+    before_action :require_user, except: [:show, :index, :home]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
+
 
     def index
         @food_budgets = FoodBudget.all
     end
 
-    def create
-        
+    def new
+        @food_budget = FoodBudget.new
     end
 
-    def new
-        
+    def create
+        @food_budget = FoodBudget.new(whitelist)
+        @food_budget.user = current_user
+        if @food_budget.save
+            flash[:notice] = "Budget amount saved successfully"
+            redirect_to food_budgets_path
+        else
+            render "new"
+        end
     end
 
     def edit
@@ -27,6 +38,23 @@ class FoodBudgetsController < ApplicationController
     def destroy
         
     end
+
+private
+
+def find_by_id
+    @food_budget = FoodBudget.find(params[:id])
+end
+
+def require_same_user
+    if current_user != @food_budget.user
+       flash[:alert] = "You can only edit or delect your own food budget" 
+       redirect_to @food_budget
+    end
+end
+
+def whitelist
+    params.require(:food_budget).permit(:amount)
+end
 
 
 end
