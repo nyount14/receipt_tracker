1e1513e59a9f648c528ddf871253fbd512513c87
nyount14 <90732548+nyount14@users.noreply.github.com>
2022-04-07 20:02:06 -0500

    now rendering transactions and budget on client's profile page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
         if user && user.authenticate(params[:session][:password])
             session[:user_id] = user.id
             flash[:notice] = "logged in successfully"
-            redirect_to user.budget
+            redirect_to user
         else
             flash.now[:alert] = "Email and/or password is incorrect"
             render 'new'
